51743134614047d2aace0943a2b143e9f5ab1b5d
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2025-10-21 18:25:17 +0500

    working on student registration

--- Project/app/verify-account/page.tsx
+++ Project/app/verify-account/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { OTPVerification } from "@/components/OTPVerificationPage/OTPVerification";
 
-const page = () => {
+export default async function OTPPage(props: {
+  searchParams: Promise<{ [key: string]: string | null }>
+}) {
+  const searchParams = await props.searchParams
+  const email = searchParams.email
   return (
-    <div>page</div>
+    <main className="min-h-screen flex items-center justify-center bg-background p-4">
+      <OTPVerification email={email} />
+    </main>
   )
 }
-
-export default page
