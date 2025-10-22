4fa16be627b4550394cf48acad82b62a4a1578de
Muzamilhussain258g <muzamilhussain258g@gmail.com>
2025-10-23 01:09:29 +0500

    past paper upload modal on user dashboard

--- Project/app/dashboard/past-papers/page.tsx
+++ Project/app/dashboard/past-papers/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useState } from "react";
+import { toast } from "@/hooks/use-toast";
 import Header from "@/components/dashboard/common/Header";
 import SearchFilters from "@/components/dashboard/PastPapersPage/SearchFilters";
 import ResultsHeader from "@/components/dashboard/PastPapersPage/ResultsHeader";
 import PapersGrid from "@/components/dashboard/PastPapersPage/PapersGrid";
 import { PastPaper } from "@/types/past-paper";
+import UploadDocumentModal from "@/components/dashboard/PastPapersPage/uploadDocumentModal";
+import { set } from "mongoose";
 
 const pastPapers: PastPaper[] = [
   {
@@ -88,6 +91,8 @@ const semesters = ["All Semesters", "Fall", "Spring", "Summer"];
 const examTypes = ["All Types", "Final", "Midterm", "Quiz", "Assignment"];
 
 export default function PastPapersPage() {
+  const [isUploadModalDocumentOpen, setIsUploadModalDocumentOpen] = useState(false);
+
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedSubject, setSelectedSubject] = useState("All Subjects");
   const [selectedYear, setSelectedYear] = useState("All Years");
@@ -112,13 +117,29 @@ export default function PastPapersPage() {
     console.log(`Downloading paper: ${title}`);
   };
 
+  // Document actions 
+  const handleUploadDocument = () => {
+    setIsUploadModalDocumentOpen(false);
+    toast({
+      title: "Document Uploaded",
+      description: "Document has been uploaded and is pending approval.",
+    });
+  };
+
   return (
     <div className="p-2 sm:p-6 space-y-6">
       {/* Header */}
-      <Header
-        title="Past Papers Repository"
-        description="Access previous exam papers, assignments, and study materials"
-      />
+      <div className="flex justify-between items-center">
+        <Header
+          title="Past Papers Repository"
+          description="Access previous exam papers, assignments, and study materials"
+        />
+        <UploadDocumentModal
+          isOpen={isUploadModalDocumentOpen}
+          onOpenChange={setIsUploadModalDocumentOpen}
+          onUpload={handleUploadDocument}
+        />
+      </div>
 
       {/* Search and Filters */}
       <SearchFilters
